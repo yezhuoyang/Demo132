7b9f6cd7210ac399790ee927d99fa6ca87bbcd03
yezhuoyang <737475845@qq.com>
2025-04-08 09:13:17 -0700

    Print false expression

--- MyVisitor.java
+++ MyVisitor.java
@@ -10,7 +10,7 @@ public class MyVisitor extends DepthFirstVisitor {
     
     @Override
     public void visit(S n) {
-        System.out.println("Visiting an S node");
+        //System.out.println("Visiting an S node");
         // Optionally do something custom with n
         // Then be sure to visit children
         super.visit(n);
@@ -19,12 +19,22 @@ public class MyVisitor extends DepthFirstVisitor {
     @Override
     public void visit(E n) {
         System.out.println("Visiting an E node");
+        if (n.f0.choice instanceof NodeToken) {
+            NodeToken token = (NodeToken) n.f0.choice;
+            if (token.tokenImage.equals("false")) {
+                int beginLine = token.beginLine;
+                int beginColumn = token.beginColumn;
+                System.out.println("Found a FALSE token");
+                System.out.println("Line: " + beginLine + ",  Column:" + beginColumn);
+            }
+        } 
         super.visit(n);
+           // -1 for these ints means no position info is available.
     }
     
     @Override
     public void visit(L n) {
-        System.out.println("Visiting an L node");
+        //System.out.println("Visiting an L node");
         super.visit(n);
     }
     
